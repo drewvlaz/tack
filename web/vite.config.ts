bf3c4020379644e1dbcda5e23568ccd63e926777
Drew Vlasnik <drew@axlehealth.com>
2026-06-12 15:03:48 -0700

    chore: chunk build files

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,4 +4,16 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (!id.includes('node_modules')) {return;}
+          if (id.includes('framer-motion')) {return 'motion';}
+          if (id.includes('@tanstack') || id.includes('@trpc')) {return 'query';}
+          if (id.includes('/react-dom/') || id.includes('/react/')) {return 'react';}
+        },
+      },
+    },
+  },
 });
